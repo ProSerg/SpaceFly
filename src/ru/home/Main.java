6dd264508576e9db57091ffb96d1e4e31100ae6c
smarkin <markin@gollard.ru>
2016-02-09 17:39:21 +0300

    [*] test

--- src/ru/home/Main.java
+++ src/ru/home/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        BaseView view =  new BaseView();
+        System.out.print(view.getTest());
     }
 }
